f3fdbcd814e064fdeae32059e176c89f45d167ff
Andrey Drow <miupanka@gmail.com>
2025-04-28 15:54:05 +0300

    Первоначальная загрузка проекта

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // If client-side, don't polyfill Node.js modules
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
